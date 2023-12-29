939b9cdc2c1cf8ee74b741390ec1dc9423f81a91
Rohit Varma <117149172+RohitVarmaSixtyFive@users.noreply.github.com>
2023-12-29 23:01:07 +0530

    Delete bscafter.py

--- bscafter.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class BinaryMapping(Scene):
-    def construct(self):
-
-        equation = MathTex("C = (1 - q)(1 - H(p) - \\frac{1}{\\beta})")
-        self.play(Create(equation))
-        self.wait(3)
